1d3b12e586e9f5c3b75b9b249f0e7ac9dfaca314
dmitry.artiushenko <dmitry.artiushenko@intexsoft.by>
2019-06-06 14:38:21 +0300

    Define advice executable before and after the method.

--- src/main/java/com/artiushenko/springTutorial/Audience.java
+++ src/main/java/com/artiushenko/springTutorial/Audience.java
@@ -1,5 +1,7 @@
 package com.artiushenko.springTutorial;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Audience {
     public void takeSeats(){    //Перед выступлением
         System.out.println("The audience is taking their seats.");
@@ -15,5 +17,20 @@ public class Audience {
 
     public void demandRefund(){  //После неудачного выступления
         System.out.println("Boo! We want our money back!");
+    }
+
+    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+        try {
+            System.out.println("The audience is taking their seats.");
+            System.out.println("The audience is turning off their cellphones");
+            long start = System.currentTimeMillis(); // Перед выступлением
+            joinpoint.proceed(); // Вызов целевого метода
+            long end = System.currentTimeMillis(); // После выступления
+            System.out.println("CLAP CLAP CLAP CLAP CLAP");
+            System.out.println("The performance took " + (end - start)
+                    + " milliseconds.");
+        } catch (Throwable t) {
+            System.out.println("Boo! We want our money back!");
+        }
     }
 }
